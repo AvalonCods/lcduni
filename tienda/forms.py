8b55f7967ccb2b8ed1fe0756f77dc007f389d22c
AvalonCodes <163208691+AvalonCods@users.noreply.github.com>
2024-03-13 16:13:40 -0300

    Delete tienda directory

--- tienda/forms.py
+++
@@ -1,6 +0,0 @@
-from django.forms import ModelForm
-from django import forms 
-
-class ContactoForm(forms.Form):
-    correo = forms.EmailField()
-    mensaje = forms.CharField(widget= forms.Textarea)
